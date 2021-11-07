65e470725316bc7ad7e5ab4f31535492b8a870d5
B3T097 <alberto@pecuarios.com>
2021-11-07 10:27:12 -0600

    Login completo

--- src/app/pages/users/login/login.component.ts
+++ src/app/pages/users/login/login.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { user } from '../../interfacez/user.interface';
+import { DatosService } from '../../services/datos.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit(): void {
+  login: user = {
+    nombre: '',
+    correo: '',
+    password: '',
+    area: '',
+    puesto: ''
+  }
+
+  alerta: boolean = false;
+  
+  constructor( private service: DatosService,
+              public routing: Router ) { }
+
+  entrar(){
+    if ( this.login.correo == '' ) {
+      this.alerta = true;
+      setTimeout(() => {
+        this.alerta = false;
+      }, 1500);
+      return;
+    }
+
+    if ( this.login.password == '' ) {
+      this.alerta = true;
+      setTimeout(() => {
+        this.alerta = false;
+      }, 1500);
+      return;
+    }
+
+    this.service.VaidateLogin( this.login )
+      .subscribe( validacion => {
+        console.log(validacion);
+        if ( !validacion.success ) {
+          this.alerta = true;
+          setTimeout(() => {
+            this.alerta = false;
+          }, 1500);
+        } else {
+          sessionStorage.setItem('login', 'true');
+          this.routing.navigate(['lecciones']);
+        }
+      } );
   }
 
 }
